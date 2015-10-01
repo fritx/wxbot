f71517aa26e64613ee530da6e3565ec3522fe35a
Fritz Lin <uxfritz@163.com>
2015-10-02 00:02:42 +0800

    - 区分识别自定义表情/图片 - 识别视频/转账/在线聊天/实时对讲 - 缩小fuck.jpeg - v0.0.4

--- preload.js
+++ preload.js
@@ -49,9 +49,11 @@ function onLogin(){
 		var $reddot = $('.web_wechat_reddot, .web_wechat_reddot_middle').last()
 		if ($reddot.length) {
 			var $chat_item = $reddot.closest('.chat_item')
-			onReddot($chat_item)
+			setTimeout(function(){
+				onReddot($chat_item)
+			}, 100)
 		}
-	}, 200)
+	}, 100)
 }
 
 function onReddot($chat_item){
@@ -65,10 +67,12 @@ function onReddot($chat_item){
 	// 自动回复 相同的内容
 	var $msg = $([
 		// '.message_system',
-		'.msg-img',
+		'.emoticon',
+		'.picture',
 		'.location',
 		'.attach',
 		'.microvideo',
+		'.video',
 		'.voice',
 		'.card',
 		'a.app',
@@ -83,6 +87,8 @@ function onReddot($chat_item){
 			text = '发毛红包'
 		} else if (ctn === '位置共享已经结束') {
 			text = '位置共享已经结束'
+		} else if (ctn === '实时对讲已经结束') {
+			text = '实时对讲已经结束'
 		} else if (ctn.match(/(.+)邀请(.+)加入了群聊/)) {
 			text = '加毛人'
 		} else {
@@ -90,13 +96,15 @@ function onReddot($chat_item){
 		}
 	} else*/
 
-	if ($msg.is('.msg-img')) {
-		if ($msg.is('.custom_emoji')) { // 用户表情
-			reply.text = '发毛表情'
-		} else {
-			// reply.text = '发毛图片'
-			reply.image = './fuck.jpeg'
-		}
+	if ($msg.is('.emoticon')) { // 自定义表情
+		var src = $msg.find('.msg-img').prop('src')
+		debug('接收', 'emoticon', src)
+		reply.text = '发毛表情'
+	} else if ($msg.is('.picture')) {
+		var src = $msg.find('.msg-img').prop('src')
+		debug('接收', 'picture', src)
+		// reply.text = '发毛图片'
+		reply.image = './fuck.jpeg'
 	} else if ($msg.is('.location')) {
 		//var src = $msg.find('.img').prop('src')
 		var desc = $msg.find('.desc').text()
@@ -113,6 +121,10 @@ function onReddot($chat_item){
 		var src = $msg.find('video').prop('src') // 限制
 		debug('接收', 'microvideo', src)
 		reply.text = '发毛小视频'
+	} else if ($msg.is('.video')) {
+		var poster = $msg.find('.msg-img').prop('src') // 限制
+		debug('接收', 'video', src)
+		reply.text = '发毛视频'
 	} else if ($msg.is('.voice')) {
 		$msg[0].click()
 		var duration = parseInt($msg.find('.duration').text())
@@ -160,6 +172,12 @@ function onReddot($chat_item){
 		})
 		if (text === '[收到了一个表情，请在手机上查看]') { // 微信表情包
 			text = '发毛表情'
+		} else if (text === '[收到一条微信转账消息，请在手机上查看]') {
+			text = '转毛帐'
+		}  else if (text === '[收到一条视频/语音聊天消息，请在手机上查看]') {
+			text = '聊jj'
+		} else if (text === '我发起了实时对讲') {
+			text = '对讲你妹'
 		} else if (text === '该类型暂不支持，请在手机上查看') {
 			text = ''
 		} else if (text.match(/(.+)发起了位置共享，请在手机上查看/)) {
@@ -168,6 +186,7 @@ function onReddot($chat_item){
 			normal = true
 		}
 		// if (normal && !text.match(/叼|屌|diao/i)) text = ''
+		debug('接收', 'text', text)
 		reply.text = text
 	}
 	debug('回复', reply)
